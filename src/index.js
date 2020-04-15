6bf499d38a5c08389a7ea3ac4a7803df137dea79
Sachin Tomar <sachin.arox@gmail.com>
2020-04-15 16:28:12 +0530

    Changes related to strict mode + version upgrade

--- src/index.js
+++ src/index.js
@@ -32,6 +32,7 @@ let loginDomainType, loginMessageType, loginDomainData;
 
 function Biconomy(provider, options) {
 	_validate(options);
+	this.isBiconomy = true;
 	this.status = STATUS.INIT;
 	this.dappId = options.dappId;
 	this.apiKey = options.apiKey;
@@ -688,10 +689,15 @@ async function handleSendTransaction(engine, payload, end) {
 				}
 			}
 		} else {
-			let error = formatMessage(RESPONSE_CODES.BICONOMY_NOT_INITIALIZED ,
-				`Decoders not initialized properly in mexa sdk. Make sure your have smart contracts registered on Mexa Dashboard`);
-			eventEmitter.emit(EVENTS.BICONOMY_ERROR, error);
-			end(error);
+			if(engine.strictMode) {
+				let error = formatMessage(RESPONSE_CODES.BICONOMY_NOT_INITIALIZED ,
+					`Decoders not initialized properly in mexa sdk. Make sure your have smart contracts registered on Mexa Dashboard`);
+				eventEmitter.emit(EVENTS.BICONOMY_ERROR, error);
+				end(error);
+			} else {
+				_logMessage("Smart contract not found on dashbaord. Strict mode is on, so falling back to normal transaction mode");
+				return engine.providerSend(payload, end);
+			}
 		}
 	} else {
 		let error = formatMessage(RESPONSE_CODES.INVALID_PAYLOAD ,
@@ -989,10 +995,14 @@ async function _init(dappId, apiKey, engine) {
 											eventEmitter.emit(EVENTS.SMART_CONTRACT_DATA_READY, dappId, engine);
 										}
 									} else {
-										engine.status = STATUS.NO_DATA;
-										eventEmitter.emit(EVENTS.BICONOMY_ERROR,
-											formatMessage(RESPONSE_CODES.SMART_CONTRACT_NOT_FOUND ,
-												`No smart contract registered for dappId ${dappId} on Mexa Dashboard`));
+										if(engine.strictMode) {
+											engine.status = STATUS.NO_DATA;
+											eventEmitter.emit(EVENTS.BICONOMY_ERROR,
+												formatMessage(RESPONSE_CODES.SMART_CONTRACT_NOT_FOUND ,
+													`No smart contract registered for dappId ${dappId} on Mexa Dashboard`));
+										} else {
+											eventEmitter.emit(EVENTS.SMART_CONTRACT_DATA_READY, dappId, engine);
+										}
 									}
 								})
 								.catch(function(error) {
@@ -1206,7 +1216,9 @@ Biconomy.prototype.login = async function(signer, cb){
 
 		console.debug(`Biconomy engine status ${engine.status}`);
 		if(engine.status != STATUS.BICONOMY_READY) {
-			return cb(formatMessage(RESPONSE_CODES.BICONOMY_NOT_INITIALIZED,'Biconomy SDK is not initialized properly'));
+			let response = formatMessage(RESPONSE_CODES.BICONOMY_NOT_INITIALIZED,'Biconomy SDK is not initialized properly');
+			if(cb) cb(response);
+			return reject(response);
 		}
 		web3.currentProvider.sendAsync({
 			jsonrpc: JSON_RPC_VERSION,
