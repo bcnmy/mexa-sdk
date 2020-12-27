4f83cad5212a17841ade4bcbb4e7714789f36b73
livingrock7 <chiragtitiya@exvillage.com>
2020-12-27 16:31:03 +0530

    test and refactor for new token support

--- src/ERC20ForwarderClient.js
+++ src/ERC20ForwarderClient.js
@@ -101,9 +101,11 @@ class ERC20ForwarderClient {
             data: data
         };
 
+        debugger;
         const feeMultiplier = await this.feeManager.getFeeMultiplier(userAddress, token);
         const tokenOracleDecimals = await this.oracleAggregator.getTokenOracleDecimals(token);
         const transferHandlerGas = await this.feeProxy.transferHandlerGas(token);
+        _logMessage(transferHandlerGas.toString());
         // todo
         // verify cost calculation
         let cost = ethers.BigNumber.from(req.txGas.toString())
