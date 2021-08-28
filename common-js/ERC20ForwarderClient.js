8a5736e704be8b5d847f9bcc16dbc1ef82a14894
tomarsachin2271 <sachin.tomar@biconomy.io>
2021-08-28 21:38:29 +0400

    Return response when txn fallback to default provider

--- common-js/ERC20ForwarderClient.js
+++ common-js/ERC20ForwarderClient.js
@@ -2,11 +2,11 @@
 
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 
+var _regenerator = _interopRequireDefault(require("@babel/runtime/regenerator"));
+
 var _classCallCheck2 = _interopRequireDefault(require("@babel/runtime/helpers/classCallCheck"));
 
 var _createClass2 = _interopRequireDefault(require("@babel/runtime/helpers/createClass"));
-
-var _regenerator = _interopRequireDefault(require("@babel/runtime/regenerator"));
 
 var _asyncToGenerator2 = _interopRequireDefault(require("@babel/runtime/helpers/asyncToGenerator"));
 
